29a2bf3c1ba1a9515d45063d46fdabee07fd526e
Terry <terryxlife@gmail.com>
2014-05-01 22:27:40 +0800

    Initial Commit

---
+++ BillSplit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BillSplit
+//
+//  Created by Wai Hoe Yuen on 5/1/14.
+//  Copyright (c) 2014 terryxlife. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BillSplit/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  BillSplit
+//
+//  Created by Wai Hoe Yuen on 5/1/14.
+//  Copyright (c) 2014 terryxlife. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end

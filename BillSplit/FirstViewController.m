9d655f9dcb3364b229b5fab17cabe7b8087f23ce
Terry <terryxlife@gmail.com>
2014-05-03 20:17:56 +0800

    Dismiss keyboard

--- BillSplit/FirstViewController.m
+++ BillSplit/FirstViewController.m
@@ -9,7 +9,15 @@
 #import "FirstViewController.h"
 
 @interface FirstViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *price1;
+@property (weak, nonatomic) IBOutlet UITextField *price2;
+@property (weak, nonatomic) IBOutlet UITextField *price3;
+@property (weak, nonatomic) IBOutlet UITextField *govTax;
+@property (weak, nonatomic) IBOutlet UITextField *servTax;
 
+@property (weak, nonatomic) IBOutlet UITextField *total;
+
+- (IBAction)calculate:(id)sender;
 @end
 
 @implementation FirstViewController
@@ -18,6 +26,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+   // [self displayTheKeyboard];
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +35,24 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void) displayTheKeyboard {
+    [self.price1 becomeFirstResponder];
+}
+
+- (void) textFieldDidEndEditing:(UITextField *) textField {
+    [textField resignFirstResponder];
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [textField resignFirstResponder];
+    return YES;
+}
+
+- (IBAction)calculate:(id)sender {
+    [self.price1 resignFirstResponder];
+    [self.price2 resignFirstResponder];
+    [self.price3 resignFirstResponder];
+    [self.govTax resignFirstResponder];
+    [self.servTax resignFirstResponder];
+}
 @end
